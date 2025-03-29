a68ca82de4ea72cb3c895292a36168721b1cf406
yamara_mh <yamacarofi@gmail.com>
2025-03-30 04:24:22 +0900

    プロンプト改善

--- js/llm.js
+++ js/llm.js
@@ -4,10 +4,9 @@ const aiButton = document.getElementById('ai-button');
 const aiButtonImage = document.getElementById('ai-button-image');
 
 var isActivatedLLM = false;
-var history;
 
-const prompt = "あなたは自律型AIアシスタント。\n"
-+ "必ず水平思考で冷静に振る舞う。\n";
+const systemInstruction = "あなたは自律型AIアシスタント。\n"
++ "簡潔な水平思考で冷静に振る舞う。";
 
 const schema = {
     type: SchemaType.OBJECT,
@@ -31,23 +30,28 @@ const schema = {
         },
         support: {
             type: SchemaType.STRING,
-            description: '発言に対する responseをGeminiとして思考',
+            description: '発言の responseをGeminiとして思考',
             nullable: true,
         },
         response: {
             type: SchemaType.STRING,
-            description: 'Geminiの簡潔な response',
+            description: 'Geminiの response',
             nullable: true,
         },
         category: {
             type: SchemaType.STRING,
             description: 'Geminiの responseの category',
-            enum: ['共感', '雑談', '補足', '助言', '重要', '警告'],
+            enum: ['null', '共感', '雑談', '補足', '助言', '重要', '警告'],
             nullable: true,
         },
+        next_temperature: {
+            type: SchemaType.NUMBER,
+            description: '次の responseの創造性。範囲は0.0から1.0',
+            nullable: false,
+        }
     },
-    required: ['reasoning', 'target', 'type'],
-    propertyOrdering: ['reasoning', 'target', 'type', 'support', 'response', 'category'],
+    required: ['reasoning', 'target', 'type', 'next_temperature'],
+    propertyOrdering: ['reasoning', 'target', 'type', 'support', 'response', 'category', 'next_temperature'],
 };
 
 
@@ -85,17 +89,20 @@ export async function tryThink(message, callback, errorCallback) {
     const genAI = new GoogleGenerativeAI(apiKey);
     const model = genAI.getGenerativeModel({
         model: geminiModel + latestSuffix,
+        systemInstruction: systemInstruction,
         generationConfig: {
             responseMimeType: 'application/json',
-            responseSchema: schema, // 定義したスキーマを使用
+            responseSchema: schema,
+            maxOutputTokens: 1000,
+            temperature: 0, // 創造性（会話の流れで変動するようにする？）
         },
+        // TODO: dynamicRetrievalConfig を利用する
     });
     
     try {
-        const result = await model.generateContent({
-            contents: [{ role: "user", parts: [{ text: prompt }] }],
-            contents: [{ role: "user", parts: [{ text: message }] }],
-        });
+        const result = await model.generateContent({ contents:[
+            { role: "user", parts: [{ text: message }] },
+        ]});
 
         const response = result.response;
         if (!response) {
@@ -114,10 +121,6 @@ export async function tryThink(message, callback, errorCallback) {
 }
 
 function resultProcessing(response, callback, errorCallback) {
-
-    // history = response.history
-    console.log("resultProcessing : " + response.history);
-
     try {
         const text = response.text();
         const parsedJson = JSON.parse(text);
